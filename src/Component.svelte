4af67a816fb44ac337fd410a87ea6547a8e6bb84
Sean Mullen <srmullen13@gmail.com>
2020-08-12 10:16:08 -0400

    initial commit

---
+++ src/Component.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let when = false;
+  export let component;
+
+  let loading;
+
+  $: if (when) {
+    load();
+  }
+
+  function load() {
+    loading = import(component);
+  }
+</script>
+
+{#if when}
+  {#await loading then { default: Component }}
+    <Component />
+  {/await}
+{/if}
